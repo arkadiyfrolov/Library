bc2db2a9e53a59b9a2fe5ad67a92bdb482ace7b4
arkadiyfrolov <43810598+arkadiyfrolov@users.noreply.github.com>
2018-10-17 14:33:25 +0300

    Add files via upload
    Удаление папки

--- Library-master/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ Library-master/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -67,6 +67,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(69, 76);
             this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 5;
             // 
